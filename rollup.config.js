932c087a8527d5a229308e83fb0fd9486f8884c7
jfnl88 <89284758+jfnl88@users.noreply.github.com>
2023-08-23 18:35:07 -0400

    Bumped dep versions.
    - Externalized deps.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import pkg from './package.json';
 import resolve from '@rollup/plugin-node-resolve';
-import eslint from '@rollup/plugin-eslint'; 
+//import eslint from '@rollup/plugin-eslint'; 
 
 export default [
   {
@@ -10,12 +10,13 @@ export default [
       name: pkg.name,
       file: pkg.module,
     },
+    external: ['@fizz/cities-list', '@fizz/en-inflectors', '@fizz/en-lexicon', '@fizz/humannames'],
     plugins: [
       resolve(), // so Rollup can find external modules
-      eslint({ 
+      /*eslint({ 
         exclude: ['./node_modules/**', './src/style/**'], 
         fix: true,
-      }),
+      }),*/
     ],
   },
 ];
